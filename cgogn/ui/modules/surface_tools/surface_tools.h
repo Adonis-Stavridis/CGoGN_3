b065c6d29a5e28f7d2c0736a5c4057ae3893abfa
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-07-28 11:03:01 +0200

    bevel_mesh v0.5 (face separation)

--- cgogn/ui/modules/surface_tools/surface_tools.h
+++ cgogn/ui/modules/surface_tools/surface_tools.h
@@ -244,7 +244,7 @@ private:
 				}));
 	}
 
-	void set_position(std::vector<Vec3> vec, float array[3])
+	void set_position(const std::vector<Vec3>& vec, float array[3])
 	{
 		array[0] = 0.0f;
 		array[1] = 0.0f;
@@ -480,6 +480,39 @@ private:
 		}
 
 		mesh_provider_->emit_cells_set_changed(selected_mesh_, p.selected_edges_set_);
+	}
+
+	void bevel_mesh(MESH& m)
+	{
+		Parameters& p = parameters_[&m];
+
+		foreach_cell(m, [&](Face f) {
+			std::vector<Vec3> face_vertices;
+			float centre[3];
+
+			foreach_incident_vertex(m, f, [&](Vertex v) -> bool {
+				face_vertices.push_back(value<Vec3>(*p.mesh_, p.vertex_position_, v));
+				return true;
+			});
+			set_position(face_vertices, centre);
+
+			Face new_face = add_face(m, face_vertices.size(), true);
+			Dart new_face_dart = new_face.dart;
+			for (Vec3 val : face_vertices)
+			{
+				Vec3 scaleVec(centre[0] - val.x(), centre[1] - val.y(), centre[2] - val.z());
+				scaleVec *= 0.25f;
+				value<Vec3>(*p.mesh_, p.vertex_position_, Vertex(new_face_dart)) = val + scaleVec;
+				new_face_dart = phi1(m, new_face_dart);
+			}
+
+			remove_face(static_cast<CMap1&>(m), CMap1::Face(f.dart), true);
+
+			return true;
+		});
+
+		mesh_provider_->emit_attribute_changed(selected_mesh_, p.vertex_position_.get());
+		mesh_provider_->emit_connectivity_changed(selected_mesh_);
 	}
 
 	void extrude(MESH& m)
@@ -1051,6 +1084,10 @@ protected:
 							extrude(*p.mesh_);
 					}
 				}
+
+				ImGui::Separator();
+				if (ImGui::Button("Bevel Mesh"))
+					bevel_mesh(*p.mesh_);
 			}
 		}
 
